26e2f1af1b88db5d68d9981a9b6c97172312e653
StephenWenceslao <stephenwenceslao@gmail.com>
2020-12-31 14:26:59 +0800

    Initial commit

---
+++ src/API.ts
@@ -0,0 +1,47 @@
+import { shuffleArray } from './utils';
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { 
+  answers: string[] 
+};
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard"
+};
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  try {
+    const data = await (await fetch(endpoint)).json();
+    console.log(data);  
+    return data.results.map((question: Question) => (
+      {
+        ...question,
+        answers: shuffleArray([
+          ...question.incorrect_answers, 
+          question.correct_answer
+        ])
+      }
+    ))
+  } catch (error) {
+    console.error(error)
+  }
+  
+};
